e9e289be76cdd7907a73b154103062daf7461eb5
Qubeal <103391826+Qubeall@users.noreply.github.com>
2022-04-11 02:17:57 +0000

    t5

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# task 4
-word = 'infinity'
-let = (len(word))
-print("%s has %s letters." % (word, let))
+# task 5
+print("Which of the following are the ways to format strings in Python?")
 
+print('f-strings')
 
