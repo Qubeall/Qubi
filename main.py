87eb3c846fab4b2eeabc72ae1d5e5c19b209f860
Qubeal <103391826+Qubeall@users.noreply.github.com>
2022-04-12 00:15:10 +0000

    Task 2 slice

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-# task 7
+# task 2 slice
 
-income = int(input("Enter your income: "));
-
-if 0 <= income <= 15527:
-  income = income = int(input())
-
-elif 15527 <= income <= 42708:
-  income = income * 0.15
-
-elif 42708 <= income <= 132406:
-  income = income * 0.25
-
-elif 132406 <= income:
-  income = income * 0.28
-
-else:
-   print("Please select a valid input.");
-
-print('Your tax is %d' % (income))
+series = ['Game of Thrones', 'Riverdale', 'Shameless', 'Doctor Who', 'Friends']
+print('The result is %s' % series[3:5])
+# or
+print('The result is {}'.format(series[3:5]))
+# or
+print('The result is {what}'.format(what=series[3:5]))
