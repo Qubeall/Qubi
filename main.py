93c48d6d7c6c44796bfac422f66215572683c60f
Qubeal <103391826+Qubeall@users.noreply.github.com>
2022-04-13 00:16:09 +0000

    Marathon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-#chess (log arith task1)
-k = 3
-n1 = 5 - k
-n2 = 5 + k
-a = (5 - k) * (5 + k)
-1 <= n1 <= 5
-0 <= k <= n2
-print(a)
+#marathon
+n = 85
+a1 = 17
+a2 = 13
+print(n // 17 + n // 13  - n // (17 * 13))
