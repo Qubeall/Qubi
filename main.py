7057a26f310ea18926ed1a72d81f687b4ae89779
Qubeal <103391826+Qubeall@users.noreply.github.com>
2022-04-16 01:01:26 +0000

    5 task 01

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
-# 4 task 05
-
-daily_temp_c = [20, 19, 15, 25, 27, 30, 31, 29, 26, 21,
-                19, 25, 27.5, 28, 26, 29.5, 31, 27.5, 26, 29,
-                18, 17.5, 17, 16.5, 19, 20, 25, 26.5, 27, 28,
-                20.5, 19, 25, 27.5, 28, 26, 15, 25, 27, 28]
+# 5 task 01
+names = ['Denis', 'Dima0', 'Vanya', 'Dima1', 'Dima2', 'Leha', 'Vova', 'Vadim','Bogdan', 'Yura', 'Anton', 'Artyom',]
 
 
-  
-
-def celsius_to_fahrenheit(daily_temp_c):
-   return (daily_temp_c * 9 / 5) + 32
-
-fahrenheit = list(map(celsius_to_fahrenheit, daily_temp_c))
-
-
-def temp_above_80 (fahreinheit):
-  return fahreinheit < 80
-
-days = list(filter(temp_above_80, fahrenheit))
-
-
-
-for i, name in enumerate(days, start=1):
-  print(i, name)
-
+for i, name in enumerate(names, start=1):
+  print('Student {} > {}'.format(i, name))
