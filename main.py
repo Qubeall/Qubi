61158d59f64654d1e57e403762e8ec9b34948469
Qubeal <103391826+Qubeall@users.noreply.github.com>
2022-04-18 01:14:43 +0000

    5 task 03

--- main.py
+++ main.py
@@ -2,11 +2,40 @@
 from random import randint
 
 month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
 revenue = [randint(90000, 100_000) for n in range(12)]
-print(revenue)
+cost = [randint(10000,110_000) for n in range(12)]
 
-cost = [randint(50000,60000) for n in range(12)]
-print(cost)
+def profit():
+  calculate_profit = list(map(lambda x, y: x - y, revenue, cost))
+  return calculate_profit
 
-profit
+profit = profit()
+print(profit)
+
+difference_of_profits = ['over 50 %','25% - 50%', '0% - 25%', 'less than 0%']
+wording = ['great', 'decent', 'need follow up', ' critical']
+  
+
+def procent():
+  calculate_procent = list(map(lambda x: x - y , profit, )) 
+  return calculate_procent
+
+procent = procent()
+print(procent)
+
+
+great = wording[0]
+decent = wording[1]
+need_follow_up = wording[2]
+critical = wording[3]
+
+
+ 
+    
+
+
+
+
+
+for months, profits, procents in zip(month, profit, procent):
+  print(months, profits, procents,'%')
