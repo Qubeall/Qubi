4e43d2618afa3bd4bcbeca87b133718d57a6254f
Qubeal <103391826+Qubeall@users.noreply.github.com>
2022-04-18 23:15:05 +0000

    5 task 03 done

--- adada/adada.py
+++ adada/adada.py
@@ -661,3 +661,18 @@ print(freq_counter.most_common(2))
     
     
 
+
+
+#Dictionary
+my_dict = {
+  1: 'a',
+  2: 'b'
+}
+
+print(my_dict.items())
+
+rev_dict = {v: k for k, v in my_dict.items()}
+print(rev_dict)
+
+
+
--- main.py
+++ main.py
@@ -8,29 +8,21 @@ cost = [randint(10000,110_000) for n in range(12)]
 def profit():
   calculate_profit = list(map(lambda x, y: x - y, revenue, cost))
   return calculate_profit
-
 profit = profit()
-print(profit)
 
-difference_of_profits = ['over 50 %','25% - 50%', '0% - 25%', 'less than 0%']
-wording = ['great', 'decent', 'need follow up', ' critical']
-  
 
 def procent():
-  calculate_procent = list(map(lambda x: (x[0] - x[1]) // x[0], profit)) 
+  calculate_procent = list(map(lambda x, y: (x/y)*100//1, profit, revenue)) 
   return calculate_procent
-
 procent = procent()
 
 
-nums =  [51, 45, 20, -1]
-
+nums =  procent
 criteria = {50: 'great',
-            24: 'decent',
-            -1: 'need follow up',
-            -2: 'critical'
-            
-}
+            25: 'decent',
+            0: 'need follow up',
+            -100: 'critical'
+            }
 
 words  = []
 
@@ -41,19 +33,5 @@ for num in nums:
       break
 
 
-
-
-
 for months, profits, procents, word in zip(month, profit, procent, words):
-  print(months, profits, procents,'%', word)
-
-
-  #jan 1000 --
-  #feb 1500 +50% (1500 - 1000 / 1000)
-  #mar 2250 +50% (2250 - 1500 / 1500)
-
-
-
-
-
-
+  print(months,':', profits, procents, '%',  '->', word)
