e822bd8706906d57fe4e2aa12c620b982a386d73
Qubeal <103391826+Qubeall@users.noreply.github.com>
2022-04-19 02:25:34 +0000

    7 task 01

--- adada/project.py
+++ adada/project.py
@@ -93,7 +93,27 @@ class Domino:
       except IllegalMoveError as err:
             print(err)
 
-  
+
+
+  # AI MOVE COMPUTATION
+  def get_ai_command(self):
+      move_options = [opt for opt in self.computer if opt[0] in self.snake_ends or opt[1] in self.snake_ends]
+      if move_options:
+         move_choice = choice(move_options)
+         command_right: int = self.computer.index(move_choice) + 1
+         command_left: int = command_right * - 1
+         if all((self.snake_right_end in move_choice, self.snake_left_end in move_choice)):
+              command = choice((command_right, command_left))
+         elif self.snake_right_end in move_choice:
+              command = command_right
+         else: command = command_left
+      else:
+         command = 0
+      return command
+
+
+
+  # PRE-GAME
 
   def get_distribution(self, each=7):
     while not any([double in self.stock[:each * 2] for double in self.doubles]):
@@ -106,6 +126,13 @@ class Domino:
     self.stock = self.stock[each * 2 - 1:]
     if randint(1, 2) == 2:
       self.player, self.computer = self.computer, self.player
+
+
+      
+    # IN-GAME
+  def update_snake_ends(self):
+    self.snake_right_end, self.snake_left_end = self.snake[-1][-1], self.snake[0][0]
+    self.snake_ends = [self.snake_left_end, self.snake_right_end]
 
   def get_stats(self, game, turn=None):
     print('=' * ord('F'))
@@ -122,6 +149,15 @@ enumerate(list(map(str, ([p for p in self.player]))))))))
     print(''.join(('\n' if game else '', *[chr(n) for n in (83, 116, 97, 116, 117, 115, 58)])), end=' ')
     print('Computer is about to make a move. Press Enter to continue...' if turn == 'computer' else \
            'It\'s your turn to make a move. Enter your command.\n' if game else 'The game is over. ', end='')
+
+  def check_game_run(self):
+      ends_match: bool = len({self.snake_right_end, self.snake_left_end}) == 1
+      ends_less_8: bool = reduce(lambda x,y: x + y, self.snake).count(self.snake_left_end if ends_match else -1) < 8
+      return all((self.computer, self.player, ends_less_8))
+  # /IN-GAME
+
+    
+
   def check_entry(self):
     while True:
       command = input()
@@ -136,14 +172,27 @@ enumerate(list(map(str, ([p for p in self.player]))))))))
       except BadUserInputError as err:
         print(err)
 
-  def pull_from_stock(self, turn):
-    who = self.computer if turn == 'computer' else self.player
-    who.append(self.stock.pop(randint(0, len(self.stock) - 1)))
-
+  # MOVE (USER, AI)
   def place_on_board(self, turn, command):
-    who = self.computer if turn == 'computer' else self.player
-    self.snake.insert(len(who) - 1 if command > 0 else 0, who.pop(abs(command) - 1))
+      who = self.computer if turn == 'computer' else self.player
+      what = self.flip_if_needed(who.pop(abs(command) - 1), command)
+      self.snake.insert(len(self.snake) if command > 0 else 0, what)
 
+  def flip_if_needed(self, what, command):
+      if command > 0 and self.snake_right_end !=what[0] or \
+              command < 0 and self.snake_left_end !=what[1]:
+            return [what[1], what[0]]
+      return what
+
+
+  def pull_from_stock(self, turn):
+      who = self.computer if turn == 'computer' else self.player
+      who.append(self.stock.pop(randint(0, len(self.stock) - 1)))
+  # /MOVE (USER, AI)
+
+
+
+  # GAME
   def main(self):
     self.get_distribution()
     turn = 'computer' if len(self.computer) > len(self.player) else 'player'
@@ -165,7 +214,6 @@ enumerate(list(map(str, ([p for p in self.player]))))))))
 
 my_game = Domino()
 my_game.main()
-
 
      
 
