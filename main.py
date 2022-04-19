4cb8e55f670331358fb5a253839a6765d7073dea
Qubeal <103391826+Qubeall@users.noreply.github.com>
2022-04-19 23:56:17 +0000

    8 task 01

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-#5 task 05
+#8 task 01
 
-print('What will be the next output?')
 
-print('There will be an exception "StopIteration"')
-
-letters = ['A', 'B', 'C']
-
-my_iterator = iter(letters)
-
-print(next(my_iterator))
-# 'A'
-print(next(my_iterator))
-# 'B'
-print(next(my_iterator))
-# 'C'
-print(next(my_iterator)) 
+print('The except block will be executed only if the specified exception occurred')
