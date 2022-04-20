c0f453cd92bae3f468fd0be7a74f6b814540c009
Qubeal <103391826+Qubeall@users.noreply.github.com>
2022-04-20 00:55:13 +0000

    8 task 03

--- Study/Sockets, hacker proj.py
+++ Study/Sockets, hacker proj.py
@@ -27,11 +27,9 @@ with socket.socket() as client_socket:
   
   print(response)
 
-
-#Socket
 import socket
 
-
+# 2 Вариант
 with socket.socket() as client_socket:
 
   hostname = '127.0.0.1'  # str
@@ -50,3 +48,20 @@ with socket.socket() as client_socket:
   response = response.decode()  # converting back to rea
 
   print(response)
+
+
+
+
+from functools import reduce
+from collections import Counter
+
+
+stock = [[0, 6], [3, 4], [1, 4], [4, 5], [3, 5], [1, 2], [1, 3], [0, 1], [5, 6], [6, 6], [0, 4], [1, 1], [2, 3], [0, 3], [3, 3], [2, 4], [1, 5], [2, 6], [3, 6], [0, 2], [4, 4]]
+
+
+new_stock = reduce(lambda x, y: x + y, stock)
+print(new_stock)
+
+
+new_new_stock = [item for sublist in stock for item in sublist]
+print(new_new_stock)
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-#8 task 02
-
-def enter_name():
-  try:
-      name, surname = input('Введите фамилию и имя:').split()
-     
-          
-  except ValueError:
-      print('You need to enter exactly 2 words. Try again!')
-      enter_name()
-      
-  else:
-      print('Welcome to our party,', name, surname)
-enter_name()
+#8 task 03
 
 
+try:
+ print("Hello, stranger!") 
+except NameError:
+	print("Hello,", name)
+else:
+ print("What a beautiful name you have!")
+finally:
+ print("Hope to see you soon!")
